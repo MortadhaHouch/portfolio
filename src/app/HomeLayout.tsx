4de117b4d24d7fd6604d35c35a9912f250d37649
MortadhaHouch <97769483+MortadhaHouch@users.noreply.github.com>
2024-11-23 23:45:51 +0100

    latest update

--- src/app/HomeLayout.tsx
+++ src/app/HomeLayout.tsx
@@ -3,10 +3,8 @@
 import Footer from "@/components/main/Footer";
 import Header from "@/components/main/Header";
 import localFont from "next/font/local";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ThemeProvider } from "@/providers/ThemeProvider";
-import AnimationData from "../../public/assets/animations/Animation - loading.json"
-import dynamic from "next/dynamic";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -17,9 +15,7 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 export default function HomeLayout({children}:{children:React.ReactNode}) {
-    const [isSeen,setIsSeen] = useState<boolean>(false);
     useEffect(() => {
         import('lenis').then(({ default: Lenis }) => {
             const lenis = new Lenis({
@@ -40,12 +36,6 @@ export default function HomeLayout({children}:{children:React.ReactNode}) {
             return () => lenis.destroy();
         });
     }, []);
-    useEffect(()=>{
-        setIsSeen(true);
-        setTimeout(()=>{
-            setIsSeen(false);
-        },2500)
-    },[])
     return (
         <html lang="en">
             <body
@@ -58,18 +48,6 @@ export default function HomeLayout({children}:{children:React.ReactNode}) {
                     disableTransitionOnChange={false}
                 >
                     <Header/>
-                    {
-                        isSeen && (
-                            <section
-                                style={{
-                                    clipPath:`${isSeen?"circle(100% at 50% 50%)":"circle(0% at 50% 50%)"}`,
-                                    transition:"clip-path .25s"
-                                }}
-                                className="fixed top-0 left-0 w-screen h-screen z-[60] bg-slate-600 flex justify-center items-center">
-                                <Lottie animationData={AnimationData}/>
-                            </section>
-                        )
-                    }
                     {children}
                     <Footer/>
                 </ThemeProvider>
