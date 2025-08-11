107d9721b17faf314183bc8081f1f1d2b2c92179
Mortadha Houch <97769483+MortadhaHouch@users.noreply.github.com>
2025-08-11 21:16:55 +0100

    traffic monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import HomeLayout from './HomeLayout'
 import { defaultMetadata } from './metadata'
-
+import Script from 'next/script';
 // Google Search Console verification
 const verification = {
   google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || '',
@@ -24,6 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+      <Script 
+        defer 
+        src="https://cloud.umami.is/script.js" 
+        data-website-id="3e5f32d8-a6a1-45d6-8240-0f779e05fd54"
+        strategy="afterInteractive"
+      ></Script>
         <HomeLayout>
           {children}
         </HomeLayout>
